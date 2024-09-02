d776622bbe221c804722eb50fdd9896a6219ec88
Lili Yu <lili@lilis-mbp.lan>
2024-09-02 01:06:40 -0400

    added in leaderboard table

--- backend/app/api/leaderboard.py
+++ backend/app/api/leaderboard.py
@@ -1,10 +1,12 @@
 # we should expose objects pertaining to the leaderboard here
 # we also need a service to handle the calculations
 
-from flask import Blueprint
+from flask import Blueprint, jsonify, current_app
+from ..services.firebase_services import get_leaderboard
 
 challenges_bp = Blueprint('leaderboard', __name__)
 
 @challenges_bp.route('/leaderboard', methods=['GET'])
 def get_leaderboard():
-    return "Leaderboard endpoint"
+    leaderboard = get_leaderboard(current_app.db)
+    return jsonify(leaderboard)
--- backend/app/services/firebase_services.py
+++ backend/app/services/firebase_services.py
@@ -1,6 +1,13 @@
 from firebase_admin import firestore
 
+# access user account table in firestore
 def get_user_accounts(db):
     users_ref = db.collection("user_accounts")
+    docs = users_ref.stream()
+    return [doc.to_dict() for doc in docs]
+
+# access leaderboard table in firestore
+def get_leaderboard(db):
+    users_ref = db.collection("leaderboard")
     docs = users_ref.stream()
     return [doc.to_dict() for doc in docs]
